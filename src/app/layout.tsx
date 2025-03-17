4eaf91eb17acee96744c0feecea3c72d176ecbe3
strawhatsaad <strawhatsaad123@gmail.com>
2025-03-18 01:06:01 +0500

    feat: Enhance SEO with updated metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,25 @@ const calistoga = Calistoga({
 
 export const metadata: Metadata = {
   title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  description:
+    "Muhammad Saad Anjum: Expert Full Stack Next.js Developer. Crafting high-performance web applications with Next.js, leveraging server-side rendering (SSR) and client-side interactivity. Skilled in building responsive, visually appealing frontends using Tailwind CSS. Explore my portfolio for examples of my work in Next.js, React, and modern web development technologies. Let's build your next innovative web solution.",
+  applicationName: "Muhammad Saad Anjum - Portfolio",
+  creator: "Muhammad Saad Anjum",
+  keywords: [
+    "Muhammad Saad Anjum",
+    "Muhammad Saad",
+    "Muhammad Saad Anjum Portfolio",
+    "Muhammad Saad Anjum Portfolio Website",
+    "Muhammad Saad Anjum Portfolio Website Next.js",
+    "Muhammad Saad Anjum Portfolio Website Next.js Tailwind CSS",
+    "Muhammad Saad Anjum Portfolio Website Next.js Tailwind CSS React",
+    "Muhammad Saad Anjum Portfolio Website Next.js Tailwind CSS React NextAuth",
+    "Muhammad Saad Anjum Portfolio Website Next.js Tailwind CSS React NextAuth Next.js API Routes",
+    "Muhammad Saad Anjum Portfolio Website Next.js Tailwind CSS React NextAuth Next.js API Routes MongoDB",
+    "Muhammad Saad Anjum Portfolio Website Next.js Tailwind CSS React NextAuth Next.js API Routes MongoDB",
+    "Muhammad Saad Anjum Portfolio Website Next.js Tailwind CSS React NextAuth Next.js API Routes MongoDB Prisma",
+    "Muhammad Saad Anjum Portfolio Website Next.js Tailwind CSS React NextAuth Next.js API Routes MongoDB Prisma",
+  ],
 };
 
 export default function RootLayout({
